ffca9781082d62d226e25e10642e19da2fec5bf9
Erik <erik.kvale@alwardinstitute.org>
2017-11-10 16:14:08 -0800

    Extracted processing code from BEA 'get' methods and put in new method common to all

--- bea/metadata.py
+++ bea/metadata.py
@@ -1,10 +1,12 @@
 import requests
-from pprint import pprint
 import collections
+import pandas
+from pprint import pprint
+
 
 
 BEA_API_ROOT_URL = 'https://www.bea.gov/api/data/'
-BEA_API_USER_KEY = '3924A4B4-43A0-4BE6-B131-'
+BEA_API_USER_KEY = '3924A4B4-43A0-4BE6-B131-650F0740C025'
 
 
 METADATA_METHODS_AND_URLS = {
@@ -44,7 +46,7 @@ class MetaRequestHandle:
         self.user_key = user_key
 
         if result_format != 'JSON':
-            raise ValueError("result format must be ='JSON'")
+            raise ValueError("Only 'JSON' responses are supported at this time")
         else:
             self.result_format = result_format
 
@@ -56,7 +58,7 @@ class MetaRequestHandle:
 
 
     # Metadata
-    def get_dataset_list(self, target_node='Dataset'):
+    def get_dataset_list(self, target_node='Dataset', echo_request=False):
         url = (
             '{}?&'
             'UserID={}&'
@@ -67,13 +69,23 @@ class MetaRequestHandle:
                 self.result_format
             )
         )
+        return self._process_response(url, target_node, echo_request)
+
+
+    def _process_response(self, url, target_node, echo_request):
         response = requests.get(url)
         if response.ok:
             # Decode JSON response to Python type(s)
             response = response.json()
             # Unpack results
-            target_response = self._unpack_results(response, target_node)
-            return target_response
+            target_results = self._unpack_results(response, target_node)
+            if echo_request:
+                # Unpack the request echo in the response,
+                # return the echo and results as a tuple
+                echo_request_params = self._unpack_request(response)
+                return (echo_request_params, target_results)
+            else:
+                return target_results
         else:
             raise requests.HTTPError
 
@@ -86,6 +98,16 @@ class MetaRequestHandle:
                 'target_node': 'RequestParam',
             }
         )
+        try:
+            for k, v in REQUEST_NODE_HIERARCHY.items():
+                response = response[v]
+            return response
+        except KeyError as e:
+            print("The key: {} does not exist in the response"
+                  .format(e))
+            pprint(response)
+
+
 
     def _unpack_results(self, response, target_node):
         RESULTS_NODE_HIERARCHY = collections.OrderedDict(
@@ -119,7 +141,8 @@ def get_param_values_filtered():
 
 if __name__=='__main__':
     handler = MetaRequestHandle(BEA_API_ROOT_URL, BEA_API_USER_KEY)
-    pprint(handler.get_dataset_list())
+    dataset_result = handler.get_dataset_list()
+    pprint(dataset_result)
 
 
 
