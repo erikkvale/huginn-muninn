2491902be3b2c6b9b185cc10a39dba096d69dcac
Erik Kvale <eirikval@gmail.com>
2017-12-12 08:35:09 -0800

    Update metadata.py
    Added module level description from old file.

--- bea/metadata.py
+++ bea/metadata.py
@@ -1,3 +1,29 @@
+"""
+BEA API:
+This module contains specifics based on the current documentation,
+to make calls and consume the data released by the BEA.
+
+***NOTE***
+Although the BEA (at the time of this writing) supports both XML
+and JSON responses; this module does not have XML response support
+at this time, only JSON and Python types.
+***NOTE***
+--------------------
+API Calling Limits
+--------------------
+The API has default calling limits as shown below. These limits are meant
+to protect BEA’s API and webserver infrastructure from activity that may be
+detrimental to that infrastructure and/or unfairly impede other API users.
+• 1000 API calls per minute, and/or
+• 30 errors per minute, and/or
+• 50 MB (raw data) per minute.
+Any user that exceeds the above calling limits will receive an explanatory
+error message for each API call until the per-minute cause has expired.
+The best way to avoid such errors is to design your application to call
+the API within these limits, e.g., programmatically regulate the
+frequency/size of API calls.
+"""
+
 import requests
 import collections
 from pprint import pprint
